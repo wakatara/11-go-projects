6c0385ec027be3acf2f9a1330c10182a43626217
Daryl Manning <daryl@wakatara.com>
2022-05-22 19:29:24 +0800

    Minor token and command fix.

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -22,14 +22,14 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3550035254820-3557106628021-Pl8Chp8SrMGHf71It5cEc1Mp")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03H5NNHCBS-3545419061943-2345a0e94f349d2daa117852e960f1ab9df929747079d9dca5dc79989da57c50")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3550035254820-3557106628021-eeZ5KDwFxmPl6cW5X41WNrki")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03H5NNHCBS-3545486019575-bf63ce9664c8e76f78174a4fbded6411d2dd8a7ace2d0ce04acec4444064d9fb")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("my job is <year>", &slacker.CommandDefinition{
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
 		Example:     "my yob is 2020",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
@@ -52,4 +52,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-

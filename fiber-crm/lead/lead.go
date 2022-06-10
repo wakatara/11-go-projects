6909f2880528e33b09eae3737fe58587be8bfaa1
Daryl Manning <daryl@wakatara.com>
2022-06-10 18:15:33 +0800

    Added DB and Leads API.

--- fiber-crm/lead/lead.go
+++ fiber-crm/lead/lead.go
@@ -3,34 +3,54 @@ package lead
 import (
 	"github.com/wakatara/11-go-projects/fiber-crm/database"
 
-	"gorm.io/gorm"
-	"gorm.io/driver/sqlite"
 	"github.com/gofiber/fiber"
+	_ "gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Lead struct {
 	gorm.Model
-	Name    string
-	Company string
-	Email   string
-	Phone   int
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
 }
 
 func GetLeads(c *fiber.Ctx) {
-	
+	db := database.DBConn
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
 }
 
 func GetLead(c *fiber.Ctx) {
 	id := c.Params("id")
-
-
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
 }
 
 func NewLead(c *fiber.Ctx) {
-
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
 }
 
 func DeleteLead(c *fiber.Ctx) {
-
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == "" {
+		c.Status(500).Send("No Lead found with ID delete!")
+		return
+	}
+	db.Delete(&lead)
+	c.Send("Lead deleted.")
 }
-
